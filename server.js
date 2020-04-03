87b4b3a870e3e89af26928b7b69d0abb5736bab1
aagrosse <aagrock81@yahoo.com>
2020-04-03 16:35:56 -0400

    added localhost link

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 //Routes
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}! Visit http://localhost:${PORT}/ `);
   });
